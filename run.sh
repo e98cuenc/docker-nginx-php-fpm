fa30014360e77842c9a8b7ef707f9ebcb8574ca9
Joaquín Cuenca Abela <e98cuenc@gmail.com>
2017-01-23 15:28:46 +0100

    Add xdebug

--- run.sh
+++ run.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
-docker run -d -p 80:80 -p 3306:3306 --name tutpad --cap-add=SYS_PTRACE --security-opt=apparmor:unconfined -v ${1:-/Users/cuenca/projects/core_v2}:/var/www e98cuenc/nginx-php-fpm && \
+docker run -d \
+	-p 80:80 \
+	-p 3306:3306 \
+	--name tutpad \
+	--cap-add=SYS_PTRACE \
+	--security-opt=apparmor:unconfined \
+	-v ${1:-/Users/cuenca/projects/core_v2}:/var/www \
+	--add-host=docker.local:172.16.123.1 \
+	e98cuenc/nginx-php-fpm && \
 echo "Attach with: docker exec -ti tutpad /bin/sh" && \
 echo "Follow logs: docker logs --follow tutpad"
